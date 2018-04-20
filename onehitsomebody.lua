e045e141ac1600cd915ded1e1528935e4837e6d9
retardpeopledestroyer <adamos2375@gmail.com>
2018-04-20 15:57:17 +0200

    Update onehitsomebody.lua

--- onehitsomebody.lua
+++ onehitsomebody.lua
@@ -1,7 +1,7 @@
 
 
 function nakurw(plajer, miejsce, inf)
-	if plajer:Alive() and plajer:SteamID64() == "76561198075997149" then
+	if plajer:Alive() and plajer:SteamID64() == "76561198075997149" then ---- if plajer:Alive() and plajer:SteamID64() == "HERE STEAMID" or plajer:SteamID64() == "HERE STEAMID" or plajer:SteamID64() == "HERE STEAMID" then
 		inf:ScaleDamage(100000)
 end end
 
