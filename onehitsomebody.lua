e8630b2085d348dcdeb2809ddbcbf6c568ae34c1
retardpeopledestroyer <adamos2375@gmail.com>
2018-07-04 17:12:50 +0200

    better hit

--- onehitsomebody.lua
+++ onehitsomebody.lua
@@ -2,7 +2,7 @@
 
 function nakurw(plajer, miejsce, inf)
 	if plajer:Alive() and plajer:SteamID64() == "76561198075997149" then ---- if plajer:Alive() and plajer:SteamID64() == "HERE STEAMID" or plajer:SteamID64() == "HERE STEAMID" or plajer:SteamID64() == "HERE STEAMID" then
-		inf:ScaleDamage(100000)
+		inf:Kill()
 end end
 
 hook.Add("ScalePlayerDamage","beczka", nakurw)
